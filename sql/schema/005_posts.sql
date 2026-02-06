ddd4e85bb2d10097826391b7c302b686bca38a29
morauszkia <morauszki.andras@gmail.com>
2026-02-06 15:02:43 +0100

    Fix(posts): add ON DELETE CASCADE to feed_id

--- sql/schema/005_posts.sql
+++ sql/schema/005_posts.sql
@@ -7,7 +7,7 @@ CREATE TABLE posts (
     url             TEXT UNIQUE NOT NULL,
     description     TEXT,
     published_at    TIMESTAMP,
-    feed_id         UUID NOT NULL REFERENCES feeds(id)
+    feed_id         UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
 );
 
 -- +goose Down
